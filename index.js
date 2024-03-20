b428863489160f3891ee103e8e32a9e40d9f4fe4
Isabely Nunes <163344865+inunesss@users.noreply.github.com>
2024-03-20 16:35:09 -0300

    arquivo index.js
    correção

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 console.log("Estamos aprendendo JavaScript");
 console.log("segunda linha. . . ");
-console.log("erceira linha. . .");
+console.log("erceira linha. . .");console.log("Segunda linha. . . ");
+console.log("Terceira linha. . .")
