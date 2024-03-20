65c3c4a3ca72434a1cae1e14dbf31f8b7daf4249
Isabely Nunes <163344865+inunesss@users.noreply.github.com>
2024-03-20 16:36:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 console.log("Estamos aprendendo JavaScript");
-console.log("segunda linha. . . ");
-console.log("erceira linha. . .");console.log("Segunda linha. . . ");
+("Segunda linha. . . ");
 console.log("Terceira linha. . .")
